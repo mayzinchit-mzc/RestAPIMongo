859bc28fe9dd4ed53391e7e03e87c1105727e746
May Zin Chit <70872679+mayzinchit-mzc@users.noreply.github.com>
2020-09-14 11:52:14 +0630

    Add files via upload
    modified changes

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const port = 4000;
 
 // << db setup >>
 const db = require("./db");
-const { ObjectID } = require("mongodb");
+const { request } = require("express");
+
 const dbName = "edkatestdb";
 const dbCollectionName = "User";
-
 
 // << db init >>
 
@@ -24,84 +24,74 @@ db.initialize(dbName, dbCollectionName, function(dbCollection) { // successCallb
           console.log(result);
     });
 
-    // << db CRUD routes >>
-    //read all documents
+    // read all documents
     server.get("/getusers", (request, response) => {
         // return updated list
-        // dbCollection.aggregate([
-        //     {$match:{name : {$exists : true}, address : {$exists : true}}},
-        //     {$project : {_id : 0}}
-        //     ]).toArray((error, result) => {
-        //     if (error) throw error;
-        //     response.json(result);
-        // });
         dbCollection.find().toArray((error, result) => {
             if (error) throw error;
             response.json(result);
         });
     });
-     //read one
-     server.get("/getoneuser", (request, response) => {
+    // read one
+    server.get("/getoneuser", (request, response) => {
         // const testId = request.params.id;
-        const testId = request.query.id;
-    
-        dbCollection.findOne(ObjectID(testId), (error, results) => {
+        const testId = request.query.userId;
+
+        dbCollection.findOne({ userId: testId }, (error, result) => {
             if (error) throw error;
-            // return one document
-            response.json(results);
+            // return item
+            response.json(result);
         });
-        // dbCollection.aggregate(
-        //     [
-        //       {$match:{name : {$exists : true}, address : {$exists : true}, _id : objectId("5f575145c58fcf23c44ffbe6")}}
-        //     ]
-        //   ).toArray((error, result) => {
-        //     if (error) throw error;
-        //     response.json(result);
-        // });
     });
-    //create
+    // create
     server.post("/createuser", (request, response) => {
-       
-        // response.json(request.query.name);
-        // response.json(request.query.address);
+        var lastID = 0;
+        dbCollection.find().count({}, function(error, result){
+            lastID = result;
+            console.log("lastID ", lastID);
+            const test = {
+                name: request.query.name,
+                address: request.query.address,
+                age: request.query.age,
+                gender: request.query.gender,
+                userId : "us".concat(lastID)
+            };
+            dbCollection.insertOne(test, (error, result) => { // callback of insertOne
+                if (error) throw error;
+                // return updated list
+                dbCollection.find().toArray((_error, _result) => { // callback of find
+                    if (_error) throw _error;
+                    response.json(_result);
+                });
+            });
+        });
+    });
+    // update
+    server.put("/updateuser", (request, response) => {
+        const testId = request.query.userId;
         const test = {
             name: request.query.name,
             address: request.query.address,
             age: request.query.age,
-            gender: request.query.gender
+            gender: request.query.gender,
+            userId: request.query.userId
         };
-        // const test = request.body;
-        // response.result(request.query.name);
-        dbCollection.insertOne(test, (error, result) => { // callback of insertOne
-            if (error) throw error;
-            // return updated list
-            dbCollection.find().toArray((_error, _result) => { // callback of find
-                if (_error) throw _error;
-                response.json(_result);
-            });
-        });
+        
+        // dbCollection.updateOne({ id: testId }, { $set: test }, (error, result) => {
+        //     if (error) throw error;
+        //     // send back entire updated list, to make sure frontend data is up-to-date
+        //     dbCollection.find().toArray((_error, _result) =>{
+        //         if (_error) throw _error;
+        //         response.json(_result);
+        //     });
+        // });
     });
-   //update
-   server.put("/updateuser", (request, response) => {
-    const testId = request.query.name;
-    const test = request.body;
-    console.log("Editing test: ", testId, " to be ", test);
-
-    dbCollection.updateOne({ id: testId }, { $set: test }, (error, result) => {
-        if (error) throw error;
-        // send back entire updated list, to make sure frontend data is up-to-date
-        dbCollection.find().toArray(function(_error, _result) {
-            if (_error) throw _error;
-            response.json(_result);
-        });
-    });
-
-    //delete
+    // delete
     server.delete("/deleteuser", (request, response) => {
-        const testId = request.query.id;
-        console.log("Delete test with id: ", testId);
-    
-        dbCollection.deleteOne({ id: Object(testId) }, function(error, result) {
+        const myquery = request.query.userId;
+        console.log("Delete test with id: ", myquery);
+
+        dbCollection.deleteOne({ userId : myquery }, function(error, result) {
             if (error) throw error;
             // send back entire updated list after successful request
             dbCollection.find().toArray(function(_error, _result) {
@@ -110,7 +100,6 @@ db.initialize(dbName, dbCollectionName, function(dbCollection) { // successCallb
             });
         });
     });
-});
 }, function(err) { // failureCallback
     throw (err);
 });
